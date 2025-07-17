6e1971fd28f5a660a573a3c7a25a96214fe3ae23
mallayevzayniddin09-ship-it <mallayevzayniddin09@gmail.com>
2025-07-17 19:56:20 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import asyncio
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types
 from config import BOT_TOKEN
-from handlers import register_handlers
+from handlers import handle_start, handle_other_messages
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-register_handlers(dp)
+@dp.message(commands=["start"])
+async def start_handler(message: types.Message):
+    await handle_start(message, bot)
+
+@dp.message()
+async def other_handler(message: types.Message):
+    await handle_other_messages(message, bot)
 
 async def main():
+    print("🤖 KinoBot ishga tushdi...")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
