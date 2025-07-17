846a7fed03c41c5cb740b8e5eb7cb6462571a1c2
mallayevzayniddin09-ship-it <mallayevzayniddin09@gmail.com>
2025-07-17 19:48:30 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+
+BOT_TOKEN = "7805028417:AAHK9C4QwNnBSImdOI-B77MZ_ZJdrAcSMWM"
+ADMIN_ID = 6857712473
+CHANNEL_ID = -1002121861813  # @fcmobile_uz_savdo kanalining ID raqami
+
+# Kino fayllar ro'yxati. Admin botga 1, 2, 3 kabi raqam yuborsa, tegishli kino yuboriladi.
+MOVIES = {
+    "1": "kino1.mp4",
+    "2": "kino2.mp4",
+    "3": "kino3.mp4",
+}
